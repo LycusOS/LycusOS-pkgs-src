3832a8e6664525cee2909eb6fa30b7955146e30f
sarveshrulz <sarveshkardekar@gmail.com>
2021-10-06 22:21:00 +0530

    Update :)

--- lycusos-configs/src/skel/.config/fish/config.fish
+++ lycusos-configs/src/skel/.config/fish/config.fish
@@ -3,7 +3,7 @@ if status --is-interactive
 	if test -z "$DISPLAY" -a $XDG_VTNR = 1
 		read --prompt="echo 'Do you want to startx? [Y/n]: '" prompt
 		if test -z "$prompt"
-			set prompt (string replace -r ".*" y "$prompt")
+			set prompt y
 		end
 		if test $prompt = y -o $prompt = Y
 			exec startx -- -keeptty
