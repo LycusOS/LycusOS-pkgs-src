92a779c11f8c1e9205938a7a6323cc5bf6eec21c
sarveshrulz <sarveshkardekar@gmail.com>
2021-10-27 15:45:24 +0000

    Update :)

--- makepkgs.sh
+++ makepkgs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 rm LycusOS-pkgs-repo/x86_64/*
-for i in $(ls -l | grep "^d" | awk '{print $9}'); do
+for i in $(ls -l | grep "^d" | awk '{print $9}' | sed 's|LycusOS-pkgs-repo||'); do
     cd "${i}"
     makepkg -fs
-    mv *.pkg.tar.zst ../LycusOS-pkgs-repo
+    mv *.pkg.tar.zst ../LycusOS-pkgs-repo/x86_64
     rm -rf pkg
     cd ..
 done
